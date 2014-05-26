986dcd6bc8de6f032b1284e65de2a30d200db4b9
jordanzet <jordanzet@gmail.com>
2014-05-26 12:12:07 -0500

    modificado pedido.py del modelo

--- modelo/pedido.py
+++ modelo/pedido.py
@@ -1,16 +1,24 @@
-
 class Pedido():
+
 	def __init__(self, codigo, fecha, cliente, detalle, fechaentrega, listocheck):
-		pass
+		self.codigo = codigo
+		self.fecha = fecha
+		self.cliente = cliente
+ 		self.detalle = detalle
+		self.fechaentrega = fechaentrega
+		self.total = total
+		self.listocheck = listocheck
 
-	def selecPedido():
-		pass
-
-	def listarPedidos():
-		pass
-
-	def procesarPedido():
-		pass
-
-	def terminarPedido ():
-		pass
+	def seleccionar_pedido():pass
+	def listar_pedidos():pass
+	def procesar_pedido():pass
+	def terminar_pedido ():pass
+	def total_pedido():pass
+	def cambiar_estado():pass
+	def ver_pedido():pass
+	def	mostrar_pedidos_pendientes():pass
+	def generar_venta():pass
+	def realizar_pedido():pass
+	def ver_producto():pass
+	def seleccionar_producto():pass
+	def generar_pedido():pass
