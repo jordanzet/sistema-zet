e55a30201354f1df84440748d3a956f8518f7f09
jordanzet <jordanzet@gmail.com>
2014-05-29 17:04:46 -0500

    run modificado, ejecuta la ventana principal de proyecto

---
+++ run.py
@@ -0,0 +1,22 @@
+from PyQt4 import QtCore, QtGui
+import sys
+
+class MiVentana(QtGui.QWidget):
+    def __init__(self, padre = None):
+        super(MiVentana, self).__init__(padre)
+        self.button = QtGui.QPushButton("Hola",self)
+        self.connect(self.button, QtCore.SIGNAL("clicked()"), self.say_hello)
+        self.show()
+        
+    def say_hello(self,**kwargs):
+        print "hola mundo!"
+
+app = QtGui.QApplication(sys.argv)
+
+v = MiVentana()
+v.resize(1200, 600)
+v.setWindowTitle('Sistema de Inventario Zet')
+v.show()
+app.exec_()
+
+
